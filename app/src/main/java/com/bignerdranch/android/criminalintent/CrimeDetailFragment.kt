76a7d515e3f78bc37b986eafda9fd4a51943ae8a
Eduardo Lopez <eddie56.el@gmail.com>
2023-09-23 16:26:28 -0700

    finish chapter 11

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent
 
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
@@ -1,18 +1,20 @@
 package com.bignerdranch.android.criminalintent
 
+import android.text.format.DateFormat
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.criminalintent.databinding.ListItemCrimeBinding
-import kotlinx.coroutines.NonDisposableHandle.parent
 
 class CrimeHolder (
     private val binding: ListItemCrimeBinding
 ) : RecyclerView.ViewHolder(binding.root) {
     fun bind(crime: Crime) {
         binding.crimeTitle.text = crime.title
-        binding.crimeDate.text = crime.date.toString()
+        binding.crimeDate.text = DateFormat.format("EEEE, MMMM dd, yyyy", crime.date).toString()
+//        binding.crimeDate.text = crime.date.toString()
 
         binding.root.setOnClickListener {
             Toast.makeText(
@@ -20,6 +22,12 @@ class CrimeHolder (
                 "${crime.title} clicked!",
                 Toast.LENGTH_SHORT
             ).show()
+        }
+
+        binding.crimeSolved.visibility = if (crime.isSolved) {
+            View.VISIBLE
+        } else {
+            View.GONE
         }
     }
 }
